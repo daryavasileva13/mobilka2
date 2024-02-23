d1c6d0a3a91b76f0b00bc308ff92d095963e396f
Vasileva Darya <daryavasileva_13@mail.ru>
2024-02-24 00:16:43 +0300

    editing main

--- app/src/main/java/com/example/sportscomplexapp/MainActivity.java
+++ app/src/main/java/com/example/sportscomplexapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.sportscomplexapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_relative);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView_sports_academy);
+        ImageView imageView2 = (ImageView)findViewById(R.id.imageView_legs);
+        TextView textView = (TextView)findViewById(R.id.text_opening);
+        TextView textView2 = (TextView)findViewById(R.id.text_opening_hours);
+        TextView textView3 = (TextView)findViewById(R.id.text_contacts);
+
     }
 }
